1f36f7384059eba76568de28ccc80f0f44aab989
Paulo Oliveira <paulo2782@gmail.com>
2021-10-15 11:24:45 -0300

    troca banco de dados

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var con = {
-    host     : '177.137.117.196',
-    user     : 'root',
-    port     : '5306',
-    password : '26031949Pa!@!@!',
-    database : 'inova',
+    host     : 'us-cdbr-east-03.cleardb.com',
+    user     : 'b4c40535f32942',
+    port     : '3306',
+    password : '99b23f24',
+    database : 'heroku_d8e7bafc7985189',
     charset  : 'utf8mb4'
 };
 
