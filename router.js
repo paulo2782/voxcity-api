d37d731edcb645b674e06a5548726c4b83616ec0
Paulo Oliveira <psotorressistemas@gmail.com>
2021-10-16 15:50:39 -0300

    resize image

--- router.js
+++ router.js
@@ -7,7 +7,7 @@ var router  = express.Router();
 const app   = express();
 
 const multer   = require('multer');
-const multerS3 = require('multer-s3');
+const multerS3 = require('multer-s3-transform');
 
 var mysql   = require('mysql');
 var db      = require('./db.js')
