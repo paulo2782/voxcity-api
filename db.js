45254ea8d0d6d4c5b6809f21ff954333d5363703
Paulo Oliveira <psotorressistemas@gmail.com>
2021-10-17 14:53:30 -0300

    troca servidor mysql

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
-var con = {
-    host     : 'us-cdbr-east-03.cleardb.com',
-    user     : 'b4c40535f32942',
-    port     : '3306',
-    password : '99b23f24',
-    database : 'heroku_d8e7bafc7985189',
-    charset  : 'utf8mb4'
-};
-
 // var con = {
-//     host     : '177.137.117.196',
-//     user     : 'root',
-//     port     : '5306',
-//     password : '26031949Pa!@!@!',
-//     database : 'inova',
+//     host     : 'us-cdbr-east-03.cleardb.com',
+//     user     : 'b4c40535f32942',
+//     port     : '3306',
+//     password : '99b23f24',
+//     database : 'heroku_d8e7bafc7985189',
 //     charset  : 'utf8mb4'
 // };
+
+var con = {
+    host     : '177.137.117.196',
+    user     : 'root',
+    port     : '5306',
+    password : '26031949Pa!@!@!',
+    database : 'inova',
+    charset  : 'utf8mb4'
+};
 
 module.exports = con;
